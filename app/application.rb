4de70d8b50bec2187b7b887215756ff936285c72
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-29 20:17:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     
-    time = kerne.rand (1..24)
+    time = kerne.rand(1..24)
     
-    if time = 1..12 
+    if time == 1..12 
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
